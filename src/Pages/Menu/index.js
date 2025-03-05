439d924fcb0c40cd6791228d484ab3b73f39584e
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-05 16:44:56 -0600

    Using: <NavLink>

--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -1,11 +1,75 @@
+import {Link, NavLink} from "react-router-dom";
+
 const Menu = () => {
     return (
-        <navbar>
+        <nav>
             <ul>
-                <li>menu</li>
+                {/*<li>*/}
+                {/*    <Link to={'/'}>Home</Link>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <Link to={'/blog'}>Blog</Link>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <Link to={'/profile'}>Profile</Link>*/}
+                {/*</li>*/}
+
+                {/*<li>*/}
+                {/*    <NavLink*/}
+                {/*        // className={({isActive}) => 'active'}*/}
+                {/*        style={({isActive})=> (*/}
+                {/*            {color: isActive ? 'red' : 'blue'}*/}
+                {/*        )}*/}
+                {/*        to={'/'}*/}
+                {/*    >Home</NavLink>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <NavLink to={'/blog'}>Blog</NavLink>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <NavLink to={'/profile'}>Profile</NavLink>*/}
+                {/*</li>*/}
+
+                {routes.map((route) => (
+                    <li key={route.text}>
+                        <NavLink
+                            style={({isActive}) => ({
+                                    color: isActive ? 'red' : 'blue'
+                            })}
+                            // style={({ isActive, isPending, isTransitioning }) => {
+                            //     return {
+                            //         fontWeight: isActive ? "bold" : "",
+                            //         color: isPending ? "red" : "black",
+                            //         viewTransitionName: isTransitioning ? "slide" : "",
+                            //     };
+                            // }}
+                            to={route.to}
+                        >
+                            {({ isActive, isPending, isTransitioning }) => (
+                                <span className={isActive ? "active" : "old"}>
+                                    {route.text}
+                                </span>
+                            )}
+                        </NavLink>
+                    </li>
+                ))}
             </ul>
-        </navbar>
+        </nav>
     );
 };
+
+const routes = []
+routes.push({
+    to: '/',
+    text: 'Home'
+})
+routes.push({
+    to: '/blog',
+    text: 'Blog'
+})
+routes.push({
+    to: '/profile',
+    text: 'Profile'
+})
 
 export {Menu};
